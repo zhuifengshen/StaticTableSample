0370da57a1898e2e3a4e8bce695184ccbb3ad1fa
张楚昭 <zhangchuzhao@devin.local>
2016-05-22 22:49:54 +0800

    iOS Static Table Sample first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS Static Table Sample
---
+++ StaticTableGroup/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  StaticTableGroup
+//
+//  Created by 张楚昭 on 16/5/22.
+//  Copyright © 2016年 tianxing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    @IBOutlet weak var txtUserName:UITextView!
+    
+    @IBOutlet weak var txtTextField:UITextView!
+    
+    @IBAction func login(sender:AnyObject){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
