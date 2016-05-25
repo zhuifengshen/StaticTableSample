bb8d7bd6950f7fe1fe1f4b66c297e80474ef8ab3
张楚昭 <zhangchuzhao@devin.local>
2016-05-25 09:43:09 +0800

    bingding viewController.swift, fixed can't binding widget problem

--- StaticTableGroup/ViewController.swift
+++ StaticTableGroup/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UIAlertViewDelegate {
 
     @IBOutlet weak var txtUserName:UITextView!
     
-    @IBOutlet weak var txtTextField:UITextView!
+    @IBOutlet weak var txtPassword: UITextField!
     
     @IBAction func login(sender:AnyObject){
-        
+        if txtUserName.text == "xxx" && txtPassword.text == "123"{
+            UIAlertView(title: "恭喜恭喜", message: "登录成功", delegate: self, cancelButtonTitle: "确定").show()
+        }else{
+            UIAlertView(title: "很抱歉", message: "登录失败", delegate: self, cancelButtonTitle: "确定").show()
+        }
     }
     
     override func viewDidLoad() {
