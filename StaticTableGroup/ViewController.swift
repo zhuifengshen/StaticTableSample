2e9cc6e3441d061cd24c27872967e5537fe0487a
张楚昭 <zhangchuzhao@devin.local>
2016-05-25 10:51:20 +0800

    merge code

--- StaticTableGroup/ViewController.swift
+++ StaticTableGroup/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController, UIAlertViewDelegate {
+class ViewController: UITableViewController {
 
     @IBOutlet weak var txtUserName:UITextView!
     
@@ -16,9 +16,19 @@ class ViewController: UITableViewController, UIAlertViewDelegate {
     
     @IBAction func login(sender:AnyObject){
         if txtUserName.text == "xxx" && txtPassword.text == "123"{
-            UIAlertView(title: "恭喜恭喜", message: "登录成功", delegate: self, cancelButtonTitle: "确定").show()
+//            UIAlertView(title: "恭喜恭喜", message: "登录成功", delegate: self, cancelButtonTitle: "确定").show()
+            let alertController = UIAlertController(title: "恭喜恭喜", message: "登录成功", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Cancel, handler: {(alertAction)->Void in
+                    NSLog("Tap to 确定")
+            }))
+            self.presentViewController(alertController, animated: true, completion: nil)
         }else{
-            UIAlertView(title: "很抱歉", message: "登录失败", delegate: self, cancelButtonTitle: "确定").show()
+//            UIAlertView(title: "很抱歉", message: "登录失败", delegate: self, cancelButtonTitle: "确定").show()
+            let alertController = UIAlertController(title: "很抱歉", message: "登录失败", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Cancel, handler: {(alertAction)->Void in
+                    NSLog("Tap to 确定")
+            }))
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
     }
     
